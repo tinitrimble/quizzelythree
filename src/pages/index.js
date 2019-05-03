cedb85cd43624eb86bed3fdd0913aaece9944723
Tini Trimble <tini.trimble@gmail.com>
2019-05-03 12:35:42 -0700

    removed unnecessary files from index js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 export default App;
