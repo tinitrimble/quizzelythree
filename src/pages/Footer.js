7f0b01b5085ea78663abe1eaf5ec309b4a2c1745
Tini Trimble <tini.trimble@gmail.com>
2019-05-21 14:57:40 -0700

    fixed centering on footer

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -4,10 +4,12 @@ import logo from '../images/q2.svg';
 
 const Footer = () => (
   <footer className="footer">
-    <a href="www.quizzelydoo.com/about">About</a>
-    <a href="www.quizzelydoo.com/Disclaimer">Disclaimer</a>
-    <a href="www.quizzelydoo.com/about">Click Here</a>
-    <img src={logo} className="logo" alt="logo" />
+    <div className="footerlinks">
+      <a href="www.quizzelydoo.com/about">About</a>
+      <a href="www.quizzelydoo.com/Disclaimer">Disclaimer</a>
+      <a href="www.quizzelydoo.com/about">Click Here</a>
+      <img src={logo} className="logo" alt="logo" />
+    </div>
   </footer>
 )
 
