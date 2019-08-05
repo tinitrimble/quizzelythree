a320523b314323a8b4e4e58cb44834a82e93abc1
Tini Trimble <tini.trimble@gmail.com>
2019-08-05 16:11:49 -0700

    update introquiz for styled

--- src/quiztemplate/Introquiz.js
+++ src/quiztemplate/Introquiz.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import './Introquiz.css';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
 class Introquiz extends Component {
   static propTypes = {
@@ -11,16 +11,54 @@ class Introquiz extends Component {
   }
   render() {
     return (
-      <div className="quiz-title">
-        <h1 className ="quiz-name">{this.props.quiztitle}</h1>
-        <img src={this.props.intropic}  className="Quiz-pic" alt="quizpic" />
-        <div className="sum">
+      <Introquiz.QuizTitle>
+        <Introquiz.Header>{this.props.quiztitle}</Introquiz.Header>
+        <Introquiz.Quizpic src={this.props.intropic} alt="quizpic" />
+        <Introquiz.Summary>
           {this.props.quizsummary}
-        </div>
-        <button className="begin-quiz" onClick={this.props.onClick}>Begin Quiz</button>
-      </div>
+        </Introquiz.Summary>
+        <Introquiz.Begin onClick={this.props.onClick}>Begin Quiz</Introquiz.Begin>
+      </Introquiz.QuizTitle>
     )
   }
 }
+
+Introquiz.QuizTitle = styled.div`
+  text-align: center;
+  margin: 25px 0px 25px 0px;
+  padding-bottom: 20px;
+  position: relative;
+`
+
+Introquiz.Header = styled.h1`
+  font-size: 34px;
+  text-align: center;
+  display: block;
+  color: #002921;
+  font:sans-serif;
+`
+
+Introquiz.Quizpic = styled.img`
+  border-radius: 10px;
+`
+
+Introquiz.Summary = styled.div`
+  margin: 25px 0px 25px 0px;
+  color: #3e4a48;
+`
+
+Introquiz.Begin = styled(Button)`
+  padding: 10px 15px;
+  border-radius: 10px;
+  border: 1px solid #6EC8B2;
+  font-size: 16px;
+  background-color: #fff;
+  margin-bottom: 25px;
+  color: #59605f;
+  &:hover {
+    background-color: #6EC8B2;
+    color: #fff;
+  }
+`
 
 export default Introquiz;
